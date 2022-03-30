a4809b80da5fdf6656c6bab615b71c8bbf2941cd
zakissimo <zakaria.habri@gmail.com>
2022-03-30 14:57:47 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+let dvdLogo = document.getElementById("dvd-logo");
+const dvdLogoWidth = dvdLogo.clientWidth;
+const dvdLogoHeight = dvdLogo.clientHeight;
+console.log(dvdLogoHeight, dvdLogoWidth);
+const colorPalette = [
+  "#F5E0DC",
+  "#F2CDCD",
+  "#DDB6F2",
+  "#F5C2E7",
+  "#E8A2AF",
+  "#F28FAD",
+  "#F8BD96",
+  "#FAE3B0",
+  "#ABE9B3",
+  "#B5E8E0",
+  "#96CDFB",
+  "#89DCEB",
+  "#C9CBFF",
+];
+
+function init() {
+  window.requestAnimationFrame(loop);
+}
+
+let x = 1;
+let y = 1;
+let dx = 1;
+let dy = 1;
+let paletteIdx = 0;
+const speed = 5;
+
+function changeSVGColor(color) {
+  paletteIdx = (paletteIdx + 1) % colorPalette.length;
+  dvdLogo.style.color = color;
+}
+
+function loop(timeStamp) {
+  let width = document.getElementById("background").clientWidth;
+  let height = document.getElementById("background").clientHeight;
+
+  if (x + dvdLogoWidth >= width) {
+    dx = -1;
+    changeSVGColor(colorPalette[paletteIdx]);
+  } else if (y + dvdLogoHeight >= height) {
+    dy = -1;
+    changeSVGColor(colorPalette[paletteIdx]);
+  } else if (x < 0) {
+    dx = 1;
+    changeSVGColor(colorPalette[paletteIdx]);
+  } else if (y < 0) {
+    dy = 1;
+    changeSVGColor(colorPalette[paletteIdx]);
+  }
+
+  x += dx * speed;
+  y += dy * speed;
+
+  dvdLogo.style.transform = `translate(${x}px,${y}px)`;
+
+  window.requestAnimationFrame(loop);
+}
+init();
