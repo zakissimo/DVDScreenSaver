2f74e0c1089591633e1b9b7521bad0923c303c59
zakissimo <zakaria.habri@gmail.com>
2022-03-31 09:07:36 +0200

    Single quotes

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-let dvdLogo = document.getElementById("dvd-logo");
+let dvdLogo = document.getElementById('dvd-logo');
+console.log(dvdLogo);
 // The following is hardcoded because dvdLogo.getBoundingClientRect() returns 0
 // Even though on the browser I get a value ...
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  console.log(dvdLogo.getBoundingClientRect());
+});
 const dvdLogoWidth = 250;
 const dvdLogoHeight = 113;
 
-console.log(dvdLogoWidth, dvdLogoHeight);
-console.log();
-
 const colorPalette = [
-  "#F5E0DC",
-  "#F2CDCD",
-  "#DDB6F2",
-  "#F5C2E7",
-  "#E8A2AF",
-  "#F28FAD",
-  "#F8BD96",
-  "#FAE3B0",
-  "#ABE9B3",
-  "#B5E8E0",
-  "#96CDFB",
-  "#89DCEB",
-  "#C9CBFF",
+  '#F5E0DC',
+  '#F2CDCD',
+  '#DDB6F2',
+  '#F5C2E7',
+  '#E8A2AF',
+  '#F28FAD',
+  '#F8BD96',
+  '#FAE3B0',
+  '#ABE9B3',
+  '#B5E8E0',
+  '#96CDFB',
+  '#89DCEB',
+  '#C9CBFF',
 ];
 
 function init() {
@@ -40,8 +42,8 @@ function changeSVGColor(color) {
 }
 
 function loop(timeStamp) {
-  let width = document.getElementById("background").clientWidth;
-  let height = document.getElementById("background").clientHeight;
+  let width = document.getElementById('background').clientWidth;
+  let height = document.getElementById('background').clientHeight;
 
   if (x + dvdLogoWidth >= width) {
     dx = -1;
